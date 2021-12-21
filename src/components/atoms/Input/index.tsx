1ce2716d7e9a62347a949225a95c1e4412e95ba3
jaewon <jangjaewon@jaewonui-MacBookPro.local>
2021-12-22 06:41:57 +0900

    update useState 세팅

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -4,15 +4,26 @@ import * as S from './style.input'
 
 interface InputProps {
     type: 'text' | 'password'
+    value: string
     placeholder?: string
+    onChange?: (arg: string) => void
 }
 
 export const Input: React.FC<InputProps> = props => {
-    const { type, placeholder } = props
+    const { type, placeholder, value, onChange } = props
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        onChange && onChange(e.target.value)
+    }
 
     return (
         <S.InputContainer>
-            <S.Input type={type} placeholder={placeholder}></S.Input>
+            <S.Input
+                type={type}
+                placeholder={placeholder}
+                value={value}
+                onChange={handleChange}
+            ></S.Input>
         </S.InputContainer>
     )
 }
