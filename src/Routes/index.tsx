e9e3daf9917deeb84ddc27267cc2f86739d77848
jaewon <jangjaewon@jaewonui-MacBookPro.local>
2021-12-22 10:49:46 +0900

    update Redux Provider 추가

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,13 +1,20 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
-import { GlobalStyle } from "../styles";
+import { GlobalStyle } from '../styles'
+import { Signin } from '../pages/Signin'
+import { store } from '../store'
 
 export const Routers = () => {
-  return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Routes></Routes>
-    </BrowserRouter>
-  );
-};
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <GlobalStyle />
+                <Routes>
+                    <Route path="signin" element={<Signin />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    )
+}
