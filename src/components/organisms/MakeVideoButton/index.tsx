7b4c21ba8476185e134fa1c773c22453dc4975cf
jaewon <jangjaewon@jaewonui-MacBookPro.local>
2021-12-23 03:22:30 +0900

    update route 이동 추가

--- src/components/organisms/MakeVideoButton/index.tsx
+++ src/components/organisms/MakeVideoButton/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import * as S from './style.makeVideoButton'
 
 export const MakeVideoButton: React.FC = () => {
+    const navigate = useNavigate()
+
+    function handleRouteClick() {
+        navigate('/ttv')
+    }
+
     return (
-        <S.MakeVideoButtonContainer>
+        <S.MakeVideoButtonContainer onClick={handleRouteClick}>
             <S.MakeVideoImage />
             <S.MakeVideoContent>
                 <S.MakeVideoNotice>텍스트로 만드는 A.I 영상</S.MakeVideoNotice>
