99536b20742f1e24e1bbacc09a5f6b7449ac1360
jaewon <jangjaewon@jaewonui-MacBookPro.local>
2021-12-22 15:03:19 +0900

    feat Progressbar

--- src/components/templates/SigninTemplate/index.tsx
+++ src/components/templates/SigninTemplate/index.tsx
@@ -5,17 +5,23 @@ import { SigninForm } from '../../organisms/SigninForm'
 import { CloseIcon } from '../../molecules/CloseIcon'
 import { SnsLoginForm } from '../../organisms/SnsLoginForm'
 
+import { ProgressBar } from '../../organisms/ProgressBar'
+
 export const SigninTemplate: React.FC = () => {
+    const [isStep, setIsSteop] = React.useState(1)
+
     return (
         <S.SigninTemplateContainer>
             <S.SigninForm>
+                <ProgressBar currentStep={isStep} />
+
                 <S.SigninHeader>로그인</S.SigninHeader>
                 <CloseIcon />
                 <SigninForm />
                 <SnsLoginForm />
                 <S.SignupWapper>
                     <S.SignupRecommend>회원가입하고 영상을 제작해보세요!</S.SignupRecommend>
-                    <S.SignupButton>회원가입</S.SignupButton>
+                    <S.SignupButton onClick={() => setIsSteop(isStep + 1)}>회원가입</S.SignupButton>
                 </S.SignupWapper>
             </S.SigninForm>
         </S.SigninTemplateContainer>
